97e188a7c96c2c43791e910b75b572de7283b3d0
sudix <sudix7755@gmail.com>
2018-06-11 10:16:19 +0900

    Add regions

--- main.go
+++ main.go
@@ -21,9 +21,16 @@ var (
 		"us-west-1",
 		"us-west-2",
 		"eu-west-1",
+		"ap-south-1",
 		"ap-southeast-1",
 		"ap-southeast-2",
 		"ap-northeast-1",
+		"ap-northeast-2",
+		"ca-central-1",
+		"eu-central-1",
+		"eu-west-1",
+		"eu-west-2",
+		"eu-west-3",
 		"sa-east-1",
 	}
 )
